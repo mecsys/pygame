41d82cad48955ae7581d5b4c039b592609ddedd0
Isaac Mechi <isaacmechi@gmail.com>
2011-09-13 02:12:58 -0300

    Atualização de codigo teste de jogo.
    Signed-off-by: Isaac Mechi <isaacmechi@gmail.com>

--- 18/pygameInput.py
+++ 18/pygameInput.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
 import pygame, sys, random
+from time import sleep
 from pygame.locals import *
 
 # set up pygame
@@ -19,33 +19,88 @@ GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 font = pygame.font.SysFont(None, 48)
-player = pygame.Rect(300, 100, 150, 150)
-
-# set up the text
-text = font.render('10', True, (0,255,0), (0,0,255))
-textRect = text.get_rect()
-textRect.centerx = windowSurface.get_rect().centerx
-textRect.centery = windowSurface.get_rect().centery
 
 
 #pygame.draw.rect(windowSurface, (255,0,0), (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
 
-windowSurface.blit(text, textRect)
-
-pygame.draw.rect(windowSurface, WHITE, player)
-pygame.display.update()
-
 # set up the player and food data structure
-foodCounter = 0
-NEWFOOD = 40
-FOODSIZE = 20
 RESULT = False
+START = True
+START2 = True
+QUIZ = True
+fichas = []
 
 def drawText(text, font, surface, x, y,f,b):
     textobj = font.render(text, 1, f, b)
     textrect = textobj.get_rect()
     textrect.topleft = (x, y)
-    surface.blit(textobj, textrect)
+    return textobj, textrect
+#    surface.blit(textobj, textrect)
+
+for i in range(7):
+#    fichas.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+    x, y = drawText(str(random.randint(0, 10)), font, windowSurface, random.randint(0, WINDOWWIDTH), random.randint(0, WINDOWHEIGHT), GREEN, WHITE )
+    fichas.append({'text':x, 'textrect':y})
+
+
+def init_screen():
+    print("Init_screen funciona!")
+    x, y = drawText('10', font, windowSurface, (WINDOWWIDTH / 3) - 100, (WINDOWHEIGHT / 3),(0,255,0),(255,255,255))
+    windowSurface.blit(x,y)
+    pygame.display.update()
+    sleep(1)
+
+    x, y = drawText('+', font, windowSurface, (WINDOWWIDTH / 3) - 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
+    windowSurface.blit(x,y)
+    pygame.display.update()
+    sleep(1)
+
+    x, y = drawText('20', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3),(255,0,0),(255,255,255))
+    windowSurface.blit(x,y)
+    pygame.display.update()
+    sleep(1)    
+
+    x, y = drawText('+', font, windowSurface, (WINDOWWIDTH / 3) + 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
+    windowSurface.blit(x,y)
+    pygame.display.update()
+    sleep(1)    
+
+    x, y = drawText('30', font, windowSurface, (WINDOWWIDTH / 3) + 100, (WINDOWHEIGHT / 3),(0,0,255),(255,255,255))
+    windowSurface.blit(x,y)
+
+    x, y = drawText('=', font, windowSurface, (WINDOWWIDTH / 3) + 150, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
+    windowSurface.blit(x,y)
+    pygame.display.update()
+    sleep(6)
+    
+    
+    #pygame.draw.rect(windowSurface, WHITE, player)
+
+def atualiza_screen():
+    print("Atualiza_screen funciona!")
+    x, y = drawText('10', font, windowSurface, (WINDOWWIDTH / 3) - 100, (WINDOWHEIGHT / 3),(0,255,0),(255,255,255))
+    windowSurface.blit(x,y) 
+    x, y = drawText('+', font, windowSurface, (WINDOWWIDTH / 3) - 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
+    windowSurface.blit(x,y)
+    x, y = drawText('20', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3),(255,0,0),(255,255,255))
+    windowSurface.blit(x,y)
+    x, y = drawText('+', font, windowSurface, (WINDOWWIDTH / 3) + 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
+    windowSurface.blit(x,y)
+    x, y = drawText('30', font, windowSurface, (WINDOWWIDTH / 3) + 100, (WINDOWHEIGHT / 3),(0,0,255),(255,255,255))
+    windowSurface.blit(x,y)
+    x, y = drawText('=', font, windowSurface, (WINDOWWIDTH / 3) + 150, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
+    windowSurface.blit(x,y)
+
+def quiz():
+    print("Quiz funciona!")
+    for x in fichas:
+        windowSurface.blit(x['text'],x['textrect'])
+        print(x['text'], x['textrect'])
+
+    if RESULT:
+        print("Mouse moveu!")
+        global QUIZ
+        QUIZ = False
 
 # run the game loop
 while True:
@@ -64,17 +119,20 @@ while True:
     windowSurface.fill(BLACK)
 
     # draw the player onto the surface
-    drawText('10', font, windowSurface, (WINDOWWIDTH / 3) - 100, (WINDOWHEIGHT / 3),(0,255,0),(255,255,255))
-    drawText('+', font, windowSurface, (WINDOWWIDTH / 3) - 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
-    drawText('20', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3),(255,0,0),(255,255,255))
-    drawText('+', font, windowSurface, (WINDOWWIDTH / 3) + 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
-    drawText('30', font, windowSurface, (WINDOWWIDTH / 3) + 100, (WINDOWHEIGHT / 3),(0,0,255),(255,255,255))
-    drawText('=', font, windowSurface, (WINDOWWIDTH / 3) + 150, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
-    if RESULT:
-        print("Mouse moveu!")
-        drawText('60', font, windowSurface, (WINDOWWIDTH / 3) + 200, (WINDOWHEIGHT / 3),(0,255,255),(0,0,0))
-    #pygame.draw.rect(windowSurface, WHITE, player)
+    if START:
+        init_screen()
+        START = False
+    
+    if QUIZ:
+        quiz()
+    
+    if START2:
+        print("")
+        init_screen()
+        START2 = False
 
+    if not QUIZ and START2:
+        atualiza_screen()
    
     # draw the window onto the screen
     pygame.display.update()
