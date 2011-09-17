a09ed8c926a6b60a8731241c002f5320b44ac27d
Isaac Mechi <isaacmechi@gmail.com>
2011-09-17 18:20:45 -0300

    Correcao de alguns bugs e inclusao de funcionalidades faltantes.
    Signed-off-by: Isaac Mechi <isaacmechi@gmail.com>

--- 18/mathlegal.py
+++ 18/mathlegal.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import pygame, sys, random
 from time import sleep
 from pygame.locals import *
@@ -42,45 +44,33 @@ def drawText(text, font, surface, x, y,f,b):
     textrect = textobj.get_rect()    
     textrect.topleft = (x, y)
     return textobj, textrect, text
-    print("Retangulo criado!")
-#    surface.blit(textobj, textrect)
-#18,34
 
 for i in range(7):
-#    fichas.append(pygame.Rect(random.randint(0, WINDOWWIDTH - FOODSIZE), random.randint(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
     x, y, t = drawText('8', font, windowSurface, windowSurface.get_rect().centerx, 160, GREEN, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     x, y, t = drawText('9', font, windowSurface, 100, 160, YELLOW, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     x, y, t = drawText('1', font, windowSurface, 400, 160, BLUE, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     x, y, t = drawText('3', font, windowSurface, 30, 360, RED, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     x, y, t = drawText('5', font, windowSurface, 150, 360, BLUE, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     x, y, t = drawText('2', font, windowSurface, 320, 360, RED, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     x, y, t = drawText('7', font, windowSurface, 540, 360, GREEN, WHITE )
     fichas.append({'text':x, 'textrect':y, 'num':t})
-    print(fichas[0])
 
     random.shuffle(fichas)
     
 
 def init_screen():
-    print("Init_screen funciona!")
     x, y, t = drawText('7', font, windowSurface, (WINDOWWIDTH / 3) - 100, (WINDOWHEIGHT / 3),(0,255,0),(255,255,255))
     desafio.append({'num':t, 'textrect':y})
     windowSurface.blit(x,y)
@@ -116,31 +106,31 @@ def init_screen():
     sleep(1)
 
 def init_screen_2():
-    print("Init_screen_2 funciona!")
+
     windowSurface.fill(BLACK)
+
+    x, y, t = drawText('OK, Agora o resultado ;-) !!!', font, windowSurface, windowSurface.get_rect().centerx, 20, BLUE, WHITE )
+    y.centerx = windowSurface.get_rect().centerx
+    windowSurface.blit(x,y)
 
     x, y, t = drawText('7', font, windowSurface, (WINDOWWIDTH / 3) - 100, (WINDOWHEIGHT / 3),(0,255,0),(255,255,255))
     desafio.append({'num':t, 'textrect':y})
     windowSurface.blit(x,y)
-    pygame.display.update()
     
 
     x, y, t  = drawText('+', font, windowSurface, (WINDOWWIDTH / 3) - 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
     desafio.append({'num':t, 'textrect':y})
     windowSurface.blit(x,y)
-    pygame.display.update()
    
 
     x, y, t = drawText('2', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3),(255,0,0),(255,255,255))
     desafio.append({'num':t, 'textrect':y})
     windowSurface.blit(x,y)
-    pygame.display.update()
     
 
     x, y, t = drawText('+', font, windowSurface, (WINDOWWIDTH / 3) + 50, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
     desafio.append({'num':t, 'textrect':y})
     windowSurface.blit(x,y)
-    pygame.display.update()
       
 
     x, y, t = drawText('5', font, windowSurface, (WINDOWWIDTH / 3) + 100, (WINDOWHEIGHT / 3),(0,0,255),(255,255,255))
@@ -150,15 +140,13 @@ def init_screen_2():
     x, y, t = drawText('=', font, windowSurface, (WINDOWWIDTH / 3) + 150, (WINDOWHEIGHT / 3),(0,255,0),(0,0,0))
     desafio.append({'num':t, 'textrect':y})
     windowSurface.blit(x,y)
-    pygame.display.update()
     
 
-    #Solução
+    #Solucao
     x, y, t = drawText('15', font, windowSurface, 150 , 350, BLUE, WHITE)
     desafio.append({'num':t, 'textrect':y})
     pygame.draw.rect(windowSurface, WHITE, (y.left - 10, y.top - 10, y.width + 20, y.height + 20))
     windowSurface.blit(x,y)
-    #pygame.display.update()
     
     
 
@@ -166,16 +154,34 @@ def init_screen_2():
     desafio.append({'num':t, 'textrect':y})
     pygame.draw.rect(windowSurface, WHITE, (y.left - 10, y.top - 10, y.width + 20, y.height + 20))
     windowSurface.blit(x,y)
-    #pygame.display.update()
     
 
     x, y, t = drawText('11', font, windowSurface, 350, 350, RED, WHITE)
     desafio.append({'num':t, 'textrect':y})
     pygame.draw.rect(windowSurface, WHITE, (y.left - 10, y.top - 10, y.width + 20, y.height + 20))
     windowSurface.blit(x,y)
-    #pygame.display.update()
     
+def test_fichas():
 
+    aux = int(14)
+
+    test = int(pre_result[0])
+    test += int(pre_result[1])
+    test += int(pre_result[2])
+    
+    if aux == test:
+        return
+    else:
+        windowSurface.fill(RED)
+        x, y, t = drawText('LAMENTO, VOCE NAO ACERTOU 8-( !!!', font, windowSurface, windowSurface.get_rect().centerx, 160, WHITE, BLUE )
+        y.centerx = windowSurface.get_rect().centerx
+        y.centery = windowSurface.get_rect().centery
+        pygame.draw.rect(windowSurface, WHITE, (y.left - 10, y.top - 10, y.width + 20, y.height + 20))
+        windowSurface.blit(x,y)
+        pygame.display.update()
+        sleep(10)
+        sys.exit(0)
+           
 
 
 def final():
@@ -189,16 +195,14 @@ def test_click(x, y):
         for cont in desafio:
             if cont['textrect'].collidepoint(x,y):
                 aux = int(cont['num'])
-                print(aux)
 
                 test = int(pre_result[0])
                 test += int(pre_result[1])
                 test += int(pre_result[2])
-                print(test)
 
                 if aux == test:
                     windowSurface.fill(WHITE)
-                    x, y, t = drawText('PARABÉNS VOCÊ GANHOU ;-) !!!', font, windowSurface, windowSurface.get_rect().centerx, 160, BLUE, RED )
+                    x, y, t = drawText('PARABENS VOCE GANHOU ;-) !!!', font, windowSurface, windowSurface.get_rect().centerx, 160, BLUE, RED )
                     y.centerx = windowSurface.get_rect().centerx
                     y.centery = windowSurface.get_rect().centery
                     pygame.draw.rect(windowSurface, WHITE, (y.left - 10, y.top - 10, y.width + 20, y.height + 20))
@@ -209,7 +213,7 @@ def test_click(x, y):
                     
                 else:
                     windowSurface.fill(RED)
-                    x, y, t = drawText('LAMENTO, VOCÊ NÃO GANHOU 8-( !!!', font, windowSurface, windowSurface.get_rect().centerx, 160, WHITE, BLUE )
+                    x, y, t = drawText('LAMENTO, VOCE NAO GANHOU 8-( !!!', font, windowSurface, windowSurface.get_rect().centerx, 160, WHITE, BLUE )
                     y.centerx = windowSurface.get_rect().centerx
                     y.centery = windowSurface.get_rect().centery
                     pygame.draw.rect(windowSurface, WHITE, (y.left - 10, y.top - 10, y.width + 20, y.height + 20))
@@ -220,10 +224,9 @@ def test_click(x, y):
 
     for cont in fichas:       
         if cont['textrect'].collidepoint(x,y):
-            print("VC ACERTOU O RETANGULO!!! =>", cont['num'])
             pre_result.append(cont['num'])
-            print(pre_result)
             if len(pre_result) == 3:
+                test_fichas()
                 global QUIZ
                 QUIZ = False
                 final()
@@ -232,15 +235,10 @@ def test_click(x, y):
 def quiz():
       
     for x in fichas:       
-#        windowSurface.blit(x['text'],x['textrect'])
         pygame.draw.rect(windowSurface, WHITE, (x['textrect'].left - 10, x['textrect'].top - 10, x['textrect'].width + 20, x['textrect'].height + 20))
         windowSurface.blit(x['text'], x['textrect'])
    
 
-#    if RESULT:
-#        print("Mouse moveu!")
-#        global QUIZ
-#        QUIZ = True
 
 # run the game loop
 while True:
@@ -250,11 +248,8 @@ while True:
             pygame.quit()
             sys.exit()
         
-#        if event.type == MOUSEBUTTONDOWN:
         if event.type == MOUSEBUTTONUP:
             print("Mouse call!")
-            #foods.append(pygame.Rect(event.pos[0], event.pos[1], FOODSIZE, FOODSIZE))            
-            #pass
             test_click(event.pos[0], event.pos[1])            
             
     
