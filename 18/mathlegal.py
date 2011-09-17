dd75bc4659e12658bb2ecf788456d67fab24c69a
Isaac Mechi <isaacmechi@gmail.com>
2011-09-17 18:47:11 -0300

    Alterado fonte para Arial.
    Signed-off-by: Isaac Mechi <isaacmechi@gmail.com>

--- 18/mathlegal.py
+++ 18/mathlegal.py
@@ -23,7 +23,7 @@ BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 YELLOW = (0, 255, 255)
 
-font = pygame.font.SysFont(None, 48)
+font = pygame.font.SysFont('Arial', 48)
 
 
 #pygame.draw.rect(windowSurface, (255,0,0), (textRect.left - 20, textRect.top - 20, textRect.width + 40, textRect.height + 40))
